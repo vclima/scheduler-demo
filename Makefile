31defbfca9cf1820c0cb7c28370fa0a5be1bcee4
vclima <victor.lima.314@gmail.com>
2017-09-30 20:51:09 -0300

    Final

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Macros para compilacao
 CC = gcc
-CFLAGS = -Wextra
+CFLAGS = -Wextra -g
 DIR = src
 FILENAME = $(DIR)/main.c
 TARGET = ./main
--- src/main.c
+++ src/main.c
@@ -3,10 +3,22 @@
 int main() {
   int a[5];
   int n;
+  int j;
+  int tar_int;
 
-  scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
-
+  scanf("%d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &n);
   for (int i=0; i<n; i++) {
+    tar_int=0;
+    for(j=0;j<5;j++){
+      if ((i+1)%a[j]==0){
+        tar_int++;
+        printf("%d",j);
+      }
+    }
+    if(tar_int==0){
+      printf("-");
+    }
+    printf("\n");
     /* Imprime resultado da n-esima iteracao do scheduler */
   }
 
